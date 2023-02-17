f63b58bb32223f05c93b3f4ee5dd3a87a4a8254f
Carbon Intelligence <49749397+carbon-intelligence@users.noreply.github.com>
2023-02-17 10:41:12 +0000

    Fix create with regions
    old implementation was throwing error `AttributeError: 'dict' object has no attribute '_sa_instance_state'`

--- backend/techtest/routes/articles.py
+++ backend/techtest/routes/articles.py
@@ -19,10 +19,16 @@ def get_create_articles(session):
         ])
 
     if request.method == 'POST':
-        new = Article(**request.get_json())
-        session.add(new)
+        req_json = request.get_json()
+        regions_json = req_json.pop('regions', [])
+        new_article = Article(**req_json)
+        regions = session.query(Region).filter(
+            Region.id.in_([r['id'] for r in regions_json])
+        ).all()
+        new_article.regions = [r for r in regions]
+        session.add(new_article)
         session.commit()
-        return jsonify(new.asdict(follow=['regions', 'author']))
+        return jsonify(new_article.asdict(follow=['regions', 'author']))
 
 
 @app.route('/articles/<id>/', methods=['GET', 'PUT', 'DELETE'])
